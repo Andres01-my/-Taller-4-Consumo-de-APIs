adcfec6032b13a08cc57ee6e49a8477bbda65b25
Andres Mesa Yepes <andresmesay@gmail.com>
2026-04-13 21:48:22 -0500

    actualizar

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taller_motos';
 
-app.use(cors());
+app.use(cors({
+  origin: "*", // o tu dominio de frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
